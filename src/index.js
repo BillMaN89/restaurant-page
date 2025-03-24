6dd38ff3af5a55a7f3283d45315dbcb2b44e7e72
BillMaN89 <mpouloukos-synas@hotmail.com>
2025-03-24 13:55:40 +0200

    fix page transition, finish styling

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ import HomePage from "./modules/home.js";
 import Menu, { menuItems } from "./modules/menu.js";
 import contactInfo from "./modules/contact.js";
 
+
 function LoadPage(pageFunction) {
     const content = document.getElementById("content");
+  
     content.style.opacity = "0";
-
+    content.style.transform = "translateX(20px)";
+  
     setTimeout(() => {
-        content.textContent = "";
-        content.appendChild(pageFunction());
-
-        content.style.opacity = "1";
-    }, 200);
-
-}
+      content.textContent = "";
+      content.appendChild(pageFunction());
+  
+      content.style.opacity = "1";
+      content.style.transform = "translateX(0)";
+    }, 300);
+  }
+  
 
 //button listeners
 const homeBtn = document.getElementById("homeBtn");
